9ea160ff8253d2a136b7f3802d3910f97bd8cc4c
Immanuel Shikanda <shikandaimmanuel5@gmail.com>
2023-10-27 09:22:11 +0300

    Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,58 @@
-import LandingNav from "./components/nav/landingnav/landing-nav"
-import Footer from "./components/footer/footer"
-import FAQs from "./components/widgets/FAQs/faqs"
+import LandingNav from "./components/nav/landingnav/landing-nav";
+import Footer from "./components/footer/footer";
+import FAQs from "./components/widgets/FAQs/faqs";
+import Info from "./components/widgets/info/info";
+import Works from "./components/widgets/works/works";
 
-export default function LandingPage () {
+export default function LandingPage() {
   return (
     <>
       <section id="header">
-        <LandingNav/>
-        {/* Call To Action */}
+        <LandingNav />
+        <div>
+          <h1>Elevate Your Entertainment Experience !</h1>
+          <h6>
+            Discover a world of Movies, Series and TV shows - All in one place
+          </h6>
+          <div>
+            <ul>
+              <li key="idx">
+                <a
+                  href="../signup"
+                  className="py-2 px-4 text-black bg-gray-100 hover:bg-gray-200 rounded-md shadow"
+                >
+                  Get Started
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </section>
       <section id="wct">
-        {/* Why Choose TouchFlix */}
+        <header>
+          Why Choose Touchflix
+        </header>
+        <div>
+          <Info/>
+        </div>
       </section>
       <section id="hit">
-        {/* Works Components */}
+        <header>
+          How it Works
+        </header>
+        <div>
+          <Works/>
+        </div>
       </section>
       <section id="FAQs">
-        <FAQs/>
+        <header>
+          FAQ&apos;s
+        </header>
+        <div>
+          <FAQs />
+        </div>
       </section>
       <Footer />
     </>
-  )
+  );
 }
