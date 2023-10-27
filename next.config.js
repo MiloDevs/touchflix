1418831c4f54e1eede6f17616b08b45872c326cd
MiloDevs <siliconvalleyn91@gmail.com>
2023-10-27 18:28:24 +0300

    movies routing working

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental:{
-        serverActions: true,
-    },
-    images: {
-        domains: ['www.themoviedb.org']
-    }
-}
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    domains: ["www.themoviedb.org", "assets.nflxext.com"],
+  },
+};
 
 module.exports = nextConfig
